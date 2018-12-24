d8aa74713b63f36d58ca48cf9f873f2fae822ca6
sohaibaslam <sohaib.sameja@gmail.com>
2018-12-24 19:01:23 +0500

    Adedd details

--- courses/admin.py
+++ courses/admin.py
@@ -9,8 +9,9 @@ from .models import Course, Step
 class StepInline(admin.StackedInline):
     model = Step
 
-class CourseAdmin(admin.ModelAdim):
+class CourseAdmin(admin.ModelAdmin):
     inlines = [StepInline,]
+
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Step)
--- courses/views.py
+++ courses/views.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import Course
+from .models import Course, Step
 # Create your views here.
 
 
 def course_list(request):
     courses = Course.objects.all()
     return render(request, 'courses/course_list.html', {'courses': courses})
+
+
+def course_details(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+    return render(request, 'courses/course_detail.html', {'course': course})
+
+
+def step_detail(request, course_pk, step_pk):
+    step = get_object_or_404(Step, course_id=course_pk, pk=step_pk)
+    return render(request, 'courses/step_detail.html', {'step': step})
